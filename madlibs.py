03ed7307f069b48ee356767ccb8b806a24b65656
Deivid-U <deividu@nycstudents.net>
2025-02-06 10:17:59 -0500

    Madlibs beginning
    mad

---
+++ madlibs.py
@@ -0,0 +1,27 @@
+#it was a (adjective 1) day at school, and (male teacher) was preparing his (noun 1) 
+# for his (class) class. However, when class started, (name) started to (verb 1) 
+# (adverb 1), catching the teacher by surprise. "You are going to (location) 
+# for (time), because you are (adjective 2)!" (male teacher) said. 
+# In response to this, (name) grabbed the nearest (noun 2) and used it to (verb2).
+#  Then, (name) got up, and bolted out of the room at maximum speed. "That was 
+# (adjective 3), (male teacher) said" and then got back to (verb 3) for his class.
+adjective1 = input("Adjective: ")
+maleTeacher = input("Male teacher: ")
+noun1 = input("Noun: ")
+class1 = input("Class: ")
+name = input("Name: ")
+verb1 = input("Verb: ")
+adverb1 = input("Adverb: ")
+location = input("Location: ")
+time = input("Amount of time: ")
+noun2 = input("Noun: ")
+verb2 = input("Verb: ")
+adjective3 = input("Adjective: ")
+verb3 = input("Verb: ")
+
+print('It was a ' + adjective1 + ' day at school, and ' + maleTeacher + 
+' was preparing his ' + noun1 + ' for his ' + class1 + ' class. However, when class started, ' 
++ name + ' started to ' + verb1 + adverb1 + 
+' , catching the teacher by surprise. "You are going to ' + location + ' for ' + time + 
+'' ++ '' ++ ++ ++ ++ ++ ++ ++ ++)
+
