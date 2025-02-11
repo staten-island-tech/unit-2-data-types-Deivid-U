d01bac5d012dbc92043eddc63928aa74409e52fd
Deivid-U <deividu@nycstudents.net>
2025-02-11 10:14:48 -0500

    GCF and bill complete
    yippie ki yay

--- bill.py
+++ bill.py
@@ -1,4 +1,23 @@
-bill = int(input('Bill: '))
-service = input('Was the service bad, okay, good, or great?')
+bill = float(input('Bill: '))
 
 def tip(bill):
+    tipCalculated = False
+    while tipCalculated == False:
+        service = input('Was the service bad, okay, good, or great? ')
+        if service == 'great':
+            tipAmount = 0.25 * bill
+            tipCalculated = True
+        elif service == 'good':
+            tipAmount = 0.2 * bill
+            tipCalculated = True
+        elif service == 'okay':
+            tipAmount = 0.15 * bill
+            tipCalculated = True
+        elif service == 'bad':
+            tipAmount = 0
+            tipCalculated = True
+        else:
+            print('Please type in either "great", "good", "okay", or "bad".')
+    print(f'Tip: {round(tipAmount)}')
+
+tip(bill)
--- gcf.py
+++ gcf.py
@@ -1,14 +1,32 @@
-def oddOrEven(x):
+""" def oddOrEven(x):
     if x % 2 == 1:
         print('Odd')
     else:
         print('Even')
 
-def factors(x):
+number = input('Enter a number:')
+
+oddOrEven(number)
+ """
+""" def factors(x):
     factorsList = []
     for i in range (x):
         if x % (i + 1) == 0:
             factorsList.append(i + 1)
     print(factorsList)
 
-factors(15)
+number = input('Enter a number: ')
+
+factors(int(number)) """
+
+def gcf(x, y):
+    commonFactorsList = []
+    for i in range (x):
+        if x % (i + 1) == 0 and y % (i + 1) == 0:
+            commonFactorsList.append(i + 1)
+    print(max(commonFactorsList))
+
+x = int(input('Enter a number: '))
+y = int(input('Enter a number: '))
+
+gcf(x,y)
